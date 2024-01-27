753700c61d4ae2de03088ac3ebd5356c9fe2dea5
YashwanthK <yashwanthkothakota@gmail.com>
2024-01-27 11:48:09 +0530

    feat: add hero carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import HeroCarousel from '@/components/HeroCarousel';
 import Searchbar from '@/components/Searchbar';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500 ">
+      <section className="px-6 md:px-20 py-24  ">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -26,7 +27,7 @@ export default function Home() {
             </p>
             <Searchbar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
 
